51e516bcd7972d6b81087cdffbee57d0de8f18a7
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2022-02-17 12:29:20 +0100

    Update debut.php

--- projet/debut.php
+++ projet/debut.php
@@ -5,9 +5,10 @@
 	<title>Jeu du 8 américain </title>
 	<link rel="stylesheet" href="demo.css" />
 	<script type="text/javascript" src="demo.js"></script>
+	
 </head>
 <body>
-		
+		<p> <b style="text-decoration: underline">  Page de connexion pour le jeu du 8 américain </b> </p>
 	
 	
 	
@@ -41,15 +42,25 @@ if(isset($_POST['connexion'])){
                 }
             }
         }
-    }
-}
+    
 ?>
 
 <form action="connexion.php" method="post">
-    Pseudo: <input type="text" name="pseudo" />
+    Pseudo: <input type="text" name="pseudo" placeholder="..."/>
     <br />
-    Mot de passe: <input type="password" name="mdp" />
+    Mot de passe: <input type="password" name="mdp" placeholder="..."/>
     <br />
+    
+    
+<select name="niveau" >
+	<option value=""> Renseignez votre niveau </option>
+	<option value="debutant"> Débutant </option>
+	<option value="medium"> Moyen </option>
+	<option value="pro"> Professionnel </option>
+</select>
+
+    
+    
     <input type="submit" name="connexion" value="Connexion" />
 </form>
 
